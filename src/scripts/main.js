58b8310a91547ce3a13b90d5d713896b9b886d33
PieCyberPie <piecyberpie@gmail.com>
2024-05-18 21:08:30 +0300

    changed pfp

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,7 @@
-'use strict';
+"use strict";
+
+const avatar = document.querySelector(".page__me__photo");
+
+avatar.addEventListener("click", () => {
+  window.location.href = "https://t.me/piecyberpie";
+});
